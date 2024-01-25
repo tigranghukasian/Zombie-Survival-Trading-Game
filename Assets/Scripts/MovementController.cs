4980d1d8ef382ff68d82adeff4948fa0557939e3
moskmosk <tigran.ghukasian@gmail.com>
2024-01-25 18:59:37 +0400

    Added Player animations

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,6 +7,7 @@ public class MovementController : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private Animator animator;
     private Rigidbody rb;
     private Vector3 movementVector;
 
@@ -23,15 +24,42 @@ public class MovementController : MonoBehaviour
     private void Update()
     {
         movementVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0 ,Input.GetAxisRaw("Vertical"));
+        
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, transform.position);
+        Vector3 directionToMousePos = Vector3.forward;
         if (groundPlane.Raycast(ray, out float point)) {
             Vector3 rayPoint = ray.GetPoint(point);
-            Vector3 direction = rayPoint - transform.position;
-            float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.Euler(new Vector3(0, -angle + offset, 0));
+            directionToMousePos = rayPoint - transform.position;
+            float angleToRotate = Mathf.Atan2(directionToMousePos.z, directionToMousePos.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, -angleToRotate + offset, 0));
         }
 
+        Debug.DrawLine(transform.position, directionToMousePos);
+
+        float angle = Vector3.SignedAngle(Vector3.forward, directionToMousePos, Vector3.up);
+
+        // Rotate input vector by this angle
+        Vector3 rotatedInput = RotateVector(movementVector, angle);
+
+        // Use the rotated vector for animation, converting to 2D
+        Vector2 animationVector = new Vector2(rotatedInput.x, rotatedInput.z);
+        
+
+        // Use the transformed vector for animation
+        animator.SetFloat("HorizontalInput", Mathf.Clamp(animationVector.x, -1, 1));
+        animator.SetFloat("VerticalInput", Mathf.Clamp(animationVector.y, -1, 1));
+        animator.SetBool("Running", movementVector != Vector3.zero);
+       // animator.SetFloat("HorizontalInput", rotatedInputVector.x);
+       // animator.SetFloat("VerticalInput", rotatedInputVector.z);
+
+    }
+    
+    Vector3 RotateVector(Vector3 vector, float angle) {
+        float radian = angle * Mathf.Deg2Rad;
+        float cos = Mathf.Cos(radian);
+        float sin = Mathf.Sin(radian);
+        return new Vector3(vector.x * cos - vector.z * sin, 0, vector.x * sin + vector.z * cos);
     }
 
     private void FixedUpdate()
